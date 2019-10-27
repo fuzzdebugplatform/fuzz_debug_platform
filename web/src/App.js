d3e92b8ca4d0b066a1622e838f2e7d7642e94812
USTC-Han <839057430@qq.com>
2019-10-27 10:26:30 +0800

    rebuild fuzz to react and add menu

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Menu } from 'antd';
 
 import {
   BrowserRouter as Router,
+  Link,
   Switch,
   Route,
 } from "react-router-dom";
 import Home from './Home';
+import 'antd/dist/antd.css';
 import SQLFuzz from './SQLFuzz';
 import SQLDebug from './SQLDebug';
 import SQLDebug2 from './SQLDebug_2';
@@ -14,15 +17,33 @@ import SQLDebug2 from './SQLDebug_2';
 function App() {
   return (
     <Router>
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <Link to="/">
+            HOME
+          </Link>
+        </Menu.Item>
+
+        <Menu.Item key="fuzz">
+          <Link to="/sqlfuzz">
+            SQL_FUZZ
+          </Link>
+        </Menu.Item>
+
+        <Menu.Item key="debug">
+          <Link to="/sqldebug_2">
+            SQL_DEBUG
+          </Link>
+        </Menu.Item>
+
+      </Menu>
+
       <Switch>
         <Route exact path="/">
           <Home/>
         </Route>
         <Route path="/sqlfuzz">
           <SQLFuzz />
-        </Route>
-        <Route path="/sqldebug">
-          <SQLDebug />
         </Route>
         <Route path="/sqldebug_2">
           <SQLDebug2 />
--- web/src/Home.js
+++ web/src/Home.js
@@ -10,10 +10,10 @@ function Home() {
         <h1>TiDB Hackathon 2019</h1>
         <h3>基于路径统计的 SQL Bug Root Cause 自动化分析</h3>
         <div>
-          {/* <Link className="App-link" to="/sqlfuzz">SQL Fuzz</Link> */}
-          <a className="App-link" href="./sql_fuzz.html">
+           <Link className="App-link" to="/sqlfuzz">SQL Fuzz</Link>
+          {/*<a className="App-link" href="./sql_fuzz.html">
             SQL Fuzz
-          </a>
+          </a>*/}
           <Link className="App-link" to="/sqldebug">
             SQL Debug
           </Link>
